1113ef0dfc72571cf384312e857033f69fedf150
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2026-02-22 23:18:39 +0000

    feat: add gitignore, prompt

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main () {
-  printf ("We (yes we) are going to be writing a Lisp \n");
+
+static char input[2048];
+
+
+int main (int argc, char** argv) {
+  puts("Lispy Version 0.0.0.1");
+  puts("Press Ctrl+C to Exit \n");
+  
+  while (1) {
+    fputs("lispy> ", stdout);
+    fgets(input, 2048, stdin);
+
+    printf("No, you're a %s", input);
+  }
   return 0;
 }
