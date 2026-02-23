4125c5e0fff23011459420c85a2e1a4cb014c318
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2026-02-23 00:34:27 +0000

    feat: create parsers

--- parsing.c
+++ parsing.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "mpc.h"
 #ifdef _WIN32
 #include <string.h>
 
@@ -22,6 +22,21 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy    = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+  "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+  Number, Operator, Expr, Lispy);
+  
 
   puts("Lispy Version 0.0.1");
   puts("Press Ctrl+c to Exit\n");
