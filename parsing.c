e9a8e7f106967c367849ff047a07ba63b319ddf0
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2026-02-23 00:40:09 +0000

    feat: hook up parsing on the loop

--- parsing.c
+++ parsing.c
@@ -46,9 +46,20 @@ int main(int argc, char** argv) {
     char* input = readline("lispy> ");
   
     add_history(input);
-    printf("No you're a %s\n", input);
+
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
     free(input);
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
   return 0;
 }
